56cca91b04a9694b3a83e8fc26869f44046dd6d6
damoski2 <akogunoyindamola42@gmail.com>
2022-06-21 13:13:40 +0100

    Initial codes

--- pages/loader.jsx
+++ pages/loader.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { View, Image, Text, StyleSheet } from 'react-native'
+import LoadingDots from '../components/Loader/LoadingDots'
 
 const loader = () => {
   return (
-    <View>
-        <Text>dedew</Text>
+    <View className={styles.container} >
+        <View>
+            <Image 
+                source={require('../assets/images/Logo.svg')}
+            />
+            <LoadingDots />
+        </View>
     </View>
   )
 }
 
 export default loader
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    }
+})
