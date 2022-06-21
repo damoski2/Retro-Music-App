9dd5357d343bda5fd8d2c5a6bf46920e28c01e23
damoski2 <akogunoyindamola42@gmail.com>
2022-06-21 12:45:50 +0100

    initial commit

---
+++ pages/loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Image, Text, StyleSheet } from 'react-native'
+
+const loader = () => {
+  return (
+    <View>
+        <Text>dedew</Text>
+    </View>
+  )
+}
+
+export default loader
